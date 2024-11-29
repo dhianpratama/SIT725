3b356ff5301fa2efe466e5704d2ffd13d81c022d
Dhian <muthidp@gmail.com>
2024-11-29 16:16:31 +1100

    Task 3.2P

---
+++ 3.2P/src/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const config = require('./config')
+const { logger, expressWinston } = require('./utils')
+const httpStatus = require('http-status')
+
+// Initiate express instance
+const app = express()
+
+// parse json request body
+app.use(express.json())
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }))
+
+// configuration to log http request automatically
+app.use(expressWinston)
+
+// Serve static files from public folder
+// such as index.html and images
+app.use(express.static('public'))
+
+// Default response for default url
+const packageJson = require('../package.json')
+app.get('/version', (_, res) => res.send(`Math API version ${packageJson.version}`))
+
+// Handler error for other url/path. Not found.
+app.use('*', (_, res) => {
+  res.status(httpStatus.NOT_FOUND).send('Not found')
+})
+
+// Run express app
+app.listen(config.PORT, () => {
+  logger.info(`App listening on port ${config.PORT}!`)
+})
+
+module.exports = app
